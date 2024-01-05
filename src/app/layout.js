e374b441d68ecbd3d2c10a68733f3795a0a64fb8
Luis Landaeta <luis.landaeta24@gmail.com>
2024-01-05 18:44:22 -0400

    Commented NextUIProvider on layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import "./burger.css";
 import { Footer } from "./components/Footer/Footer";
 import { Navbar } from "./components/Navbar/Navbar";
-import { NextUIProvider } from "@nextui-org/react";
+// import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata = {
   title: "RNVNTU Coaching",
@@ -26,11 +26,11 @@ export default function RootLayout({ children }) {
         <script async src="https://link.msgsndr.com/js/form_embed.js"></script>
       </head>
       <body>
-        <NextUIProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </NextUIProvider>
+        {/* <NextUIProvider> */}
+        <Navbar />
+        {children}
+        <Footer />
+        {/* </NextUIProvider> */}
       </body>
     </html>
   );
